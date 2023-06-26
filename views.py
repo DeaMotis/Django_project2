a705c42c48769b9c6f1ab0c8c700860bf4be5aa0
DeaMotis <121121639+DeaMotis@users.noreply.github.com>
2023-06-26 14:06:46 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import *
+
+class AuthorList(ListView):
+    model = Author
+
