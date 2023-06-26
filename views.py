0c57fa6d73be3e96e0d1878d4e5c1377639d5040
DeaMotis <121121639+DeaMotis@users.noreply.github.com>
2023-06-26 14:05:09 +0300

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
-from .models import *
-
-class AuthorList(ListView):
-    model = Author
-
